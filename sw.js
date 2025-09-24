1aca2d39933bf294e446efe79074510726ca96ae
hyh124-R <aidanvandekamp@gmail.com>
2025-09-24 12:40:20 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,17 @@
 self.addEventListener("install", e => {
 console.log("Installing sw");
 });
+
+<script>
+if ("serviceWorker" in navigator) {
+console.log("Service Worker supported");
+navigator.serviceWorker.register("./sw.js").then(ServiceWorkerRegistration
+=> {
+console.log("SW Registered");
+console.log(ServiceWorkerRegistration);
+}).catch(error => {
+console.log("SW Registration failed");
+console.log(error);
+})
+}
+</script>
